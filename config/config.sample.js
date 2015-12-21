708f5208701db1289c1e407096b0fb224d1c0ceb
babua <onurbabacan@gmail.com>
2015-12-21 15:05:07 +0200

    added sample config

---
+++ config/config.sample.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+        telegram : {
+                "token" : 'telegramToken'
+        },
+        youtube : {
+                "key" : 'youtubeAPIKey'
+        },
+        db: 'mongodb://localhost/babbot',
+};
+
