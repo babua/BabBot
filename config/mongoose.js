ad96cc62e7e0fbf639a210d132cc1e15c230f97a
babua <onurbabacan@gmail.com>
2015-12-22 01:32:50 +0200

    removed buggy old features

--- config/mongoose.js
+++
@@ -1,8 +0,0 @@
-var config = require('./config');
-var mongoose = require('mongoose');
-
-module.exports = function(){
-        var db = mongoose.connect(config.db);
-        require('../models/game.server.model');
-        return db;
-};
--- models/game.server.model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
-
-var GameSchema = new Schema({
-	appid: Number,
-	name: String
-});
-
-mongoose.model('Game',GameSchema);
