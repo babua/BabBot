99816a0796c2709a49df29598549add7d25bc92a
babua <onurbabacan@gmail.com>
2016-05-06 19:31:55 +0300

    simple captioning module - WIP

---
+++ modules/caption.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var captionModule = {
+    commands: [
+        'cap'
+    ],
+
+    onCommand: function (command, query, platform, state) {
+
+        platform.typing(state);
+
+        var tokens = query.split("|");
+        var secondLine = "";
+        if(tokens[1] !== undefined)
+        {
+            secondLine = tokens[1];
+            secondLine = secondLine.replace(/ /g, "-");
+        }
+        // console.log(query);
+        tokens = tokens[0].split(" ");
+        console.log(tokens);
+        var image = tokens[0];
+        tokens.splice(0,1);
+        var firstLine = tokens.join("-");
+
+        //todo: check valid url
+        //todo: predefined image library
+
+        if(secondLine === "")
+        {
+            var resultText = "http://memegen.link/custom/" + firstLine +".jpg?alt=" + image;
+        } else 
+            {
+                var resultText = "http://memegen.link/custom/"+ firstLine +"/"+ secondLine +".jpg?alt=" + image;
+            }
+        
+        platform.message( resultText, state);
+
+        
+    }
+};
+
+module.exports = captionModule;
