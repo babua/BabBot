0726f5d55acd603ad29c04aff2fb7840fe1b2189
babua <onurbabacan@gmail.com>
2016-05-18 02:52:18 +0300

    added sequential operation with async for checking large text

--- modules/readability.js
+++ modules/readability.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var readability = require('node-readability'),
-    htmlToText = require('html-to-text');
-    //urlExists = require('url-exists');
+    htmlToText = require('html-to-text'),
+    async = require("async");
 
 var readabilityModule = {
     commands: [
@@ -14,33 +14,39 @@ var readabilityModule = {
         var readabilityCallback = function (err, article) {
             if (err) {
                 platform.error(err, state);
+                platform.failMessage("Bu adrese ulaşamadım " + state.message.from.first_name + ' ¯\\_(ツ)_/¯',state)
                 return;
             }
-
             if(article.content === false){
-                platform.failMessage("Bu sayfanın içeriğini ayıklayamadım " + state.message.from.first_name + ' ¯\\_(ツ)_/¯');    
+                platform.failMessage("Bu sayfanın içeriğini ayıklayamadım " + state.message.from.first_name + ' ¯\\_(ツ)_/¯',state);    
             } else {
-                var text = htmlToText.fromString(article.content, {
-                    wordwrap: false
-                });
-                if(text.length > 4096 * 3){
-                    platform.message("Bu çok uzunmuş, yollamayayım ben bunu (gözüm üstünde Koray)");
-                } else {
-                    platform.message(text,state);    
+
+                function convertToText(platform, state, articleContent, callback) {
+                    var text = htmlToText.fromString(articleContent, {
+                        wordwrap: false
+                    });
+
+                    callback(null, platform, state, text);
                 }
-                
+                function sendText(platform, state, text, callback) {
+                    if(text.length > 4096 * 3){
+                        platform.message("Bu çok uzunmuş, yollamayayım (gözüm üstünde Koray ಠ_ಠ)",state);
+                    } else {
+                        platform.message(text,state);    
+                    }
+                    callback(null, 'message sent');
+                }
+
+                async.waterfall([
+                    async.apply(convertToText, platform, state, article.content),
+                    sendText
+                ], function (err, result) {
+                });
             }    
         };
         platform.typing(state);
         readability(query,readabilityCallback);
-        // urlExists(query, function(err, exists) {
-        //     if(exists)
-        //     {
-        //         readability(query,readabilityCallback);
-        //     } else {
-        //         platform.failMessage("Böyle bi URL bulamadım " + state.message.from.first_name + ' ¯\\_(ツ)_/¯');
-        //     }
-        // });
+
     }
 };
 
