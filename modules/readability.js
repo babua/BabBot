c83681b37fd59af095776c40896402f310edaeeb
babua <onurbabacan@gmail.com>
2016-05-18 01:50:14 +0300

    disabled URL checking in readability | readability stops itself if the content is longer than 3 messages

--- modules/readability.js
+++ modules/readability.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var readability = require('node-readability'),
-    htmlToText = require('html-to-text'),
-    urlExists = require('url-exists');
+    htmlToText = require('html-to-text');
+    //urlExists = require('url-exists');
 
 var readabilityModule = {
     commands: [
@@ -23,20 +23,24 @@ var readabilityModule = {
                 var text = htmlToText.fromString(article.content, {
                     wordwrap: false
                 });
-                console.log(text);
-                platform.message(text,state);
+                if(text.length > 4096 * 3){
+                    platform.message("Bu çok uzunmuş, yollamayayım ben bunu (gözüm üstünde Koray)");
+                } else {
+                    platform.message(text,state);    
+                }
+                
             }    
         };
         platform.typing(state);
-
-        urlExists(query, function(err, exists) {
-            if(exists)
-            {
-                readability(query,readabilityCallback);
-            } else {
-                platform.failMessage("Böyle bi URL bulamadım " + state.message.from.first_name + ' ¯\\_(ツ)_/¯');
-            }
-        });
+        readability(query,readabilityCallback);
+        // urlExists(query, function(err, exists) {
+        //     if(exists)
+        //     {
+        //         readability(query,readabilityCallback);
+        //     } else {
+        //         platform.failMessage("Böyle bi URL bulamadım " + state.message.from.first_name + ' ¯\\_(ツ)_/¯');
+        //     }
+        // });
     }
 };
 
